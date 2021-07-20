c107274bf4da8115527c9cf8550b48221b10dfca
schalkdaniel <d-schalk@t-online.de>
2021-07-20 13:01:49 +0200

    update analyse.R

--- eq1/analyse.R
+++ eq1/analyse.R
@@ -47,16 +47,37 @@ df_aggr = df_res %>%
   group_by(task, learner, iteration, tset) %>%
   summarize(classif.auc = mean(classif.auc), microseconds = mean(microseconds))
 
-sf = function(x) max(x) - x^(-2)
-stretch = function() scales::trans_new("one_over", sf, sf)
+sf  = function(x, p = 10) 1 - (1 - (0.2 * x)^p)^(1/p)
+sfi = function(x, p = 10) 5 * (1 - (1 - x)^p)^(1/p)
+sfb = function(x, p = 10) {
+  bl = sf(x[1], p)
+  bu = sf(x[2], p)
+  sfi(seq(bl, bu, length.out = 4L))
+}
+stretch_trans = function() scales::trans_new("stretch", sf, sfi, sfb, )
+formatFun = function(x) sprintf("%.3f", x)
 
-ggplot(mapping = aes(x = microseconds, y = classif.auc, color = learner)) +
-  geom_line(data = df_res %>% filter(tset == "test"), aes(group = paste0(learner, fold)), alpha = 0.3) +
-  geom_line(data = df_aggr %>% filter(tset == "test")) +
+ggplot(data = df_res %>% filter(tset == "test"), mapping = aes(x = microseconds / 1e6, y = classif.auc, color = learner)) +
+  geom_line(aes(group = paste0(learner, fold)), alpha = 0.3, size = 0.6) +
+  geom_line(data = df_aggr %>% filter(tset == "test"), size = 1.2) +
   geom_point(data = df_smry, mapping = aes(x = 0, y = auc_best)) +
-  #scale_y_continuous(trans = "") +
+  scale_y_continuous(trans = "stretch", labels = formatFun) +
+  xlab("Seconds") +
+  ylab("AUC") +
   ggsci::scale_color_uchicago() +
   facet_wrap(. ~ task, nrow = 2, scales = "free")
+
+
+
+plot(x, 1 - (1 - x^50)^(1/60), type = "l")
+
+x = seq(0.5, 1, by = 0.01)
+y = x
+plot(x, y^8, type = "l")
+
+ggplot(data = data.frame(x, y), aes(x = x, y = y)) +
+  geom_line() +
+  scale_y_continuous(trans = "sqrt")
 
 
 
